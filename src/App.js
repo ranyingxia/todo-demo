5f9cb1f111d3f5e5b6bb3cc6398d877143194ba2
ranyingxia <ranyingxia@ranyingxiadeMacBook-Pro.local>
2018-05-09 16:17:54 +0800

    移除无用demo

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Index from './components/Index'
 import RegexInput from './page/custom/RegexInput'
 import DynamicFieldSet from './page/custom/DynamicFieldSet'
 import Layout from './Layout';
-import PageDemo1 from './page/demo1';
-import PageDemo2 from './page/demo2';
 
 const store = createStore(reducer)
 const AppComponent = props => <Layout location={props.location}>{props.children || null}</Layout>
@@ -26,8 +24,6 @@ class App extends Component {
               <Route path="input" component={RegexInput} />
               <Route path="dynamicFieldSet" component={DynamicFieldSet} />
             </Route>
-            <Route path="demo1" component={PageDemo1} />
-            <Route path="demo2" component={PageDemo2} />
           </Route>
         </Router>
       </Provider>
